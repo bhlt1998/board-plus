5f0b26e3b79a9d48aaf2d852ba867fd75e7f1be5
bhlt1998 <zhaoderun@live.com>
2023-03-20 22:38:00 +0800

    first commit

---
+++ js/408.6f1d1aca.js
@@ -0,0 +1,2 @@
+"use strict";(self["webpackChunkboard_plus"]=self["webpackChunkboard_plus"]||[]).push([[408],{2334:function(t,n,s){s.r(n),s.d(n,{default:function(){return i}});var u=function(){var t=this,n=t._self._c;return n("div",{staticClass:"attr-list"},[n("CommonAttr")],1)},l=[],r=s(4850),o={components:{CommonAttr:r.Z}},e=o,a=s(1001),c=(0,a.Z)(e,u,l,!1,null,null,null),i=c.exports}}]);
+//# sourceMappingURL=408.6f1d1aca.js.map
---
+++ js/89.0474b538.js
@@ -0,0 +1,2 @@
+"use strict";(self["webpackChunkboard_plus"]=self["webpackChunkboard_plus"]||[]).push([[89],{866:function(t,n,s){s.r(n),s.d(n,{default:function(){return i}});var u=function(){var t=this,n=t._self._c;return n("div",{staticClass:"attr-list"},[n("CommonAttr")],1)},l=[],r=s(4850),o={components:{CommonAttr:r.Z}},e=o,a=s(1001),c=(0,a.Z)(e,u,l,!1,null,null,null),i=c.exports}}]);
+//# sourceMappingURL=89.0474b538.js.map
