6710a79736ac2a0cda66366d1168dca4cd02a8a9
bhlt1998 <zhaoderun@live.com>
2023-03-21 11:58:23 +0800

    update

--- js/664.db9e0577.js
+++ js/664.db9e0577.js
@@ -1,2 +1,2 @@
 "use strict";(self["webpackChunkboard_plus"]=self["webpackChunkboard_plus"]||[]).push([[664],{185:function(t,e,o){o.r(e),o.d(e,{default:function(){return c}});var n=function(){var t=this,e=t._self._c;return e("div",{staticClass:"attr-list"},[e("CommonAttr"),e("el-form",[e("el-form-item",{attrs:{label:"内容"}},[e("el-input",{attrs:{type:"textarea",rows:3},model:{value:t.curComponent.propValue,callback:function(e){t.$set(t.curComponent,"propValue",e)},expression:"curComponent.propValue"}})],1)],1)],1)},r=[],u=o(4850),l={components:{CommonAttr:u.Z},computed:{curComponent(){return this.$store.state.curComponent}}},s=l,a=o(1001),p=(0,a.Z)(s,n,r,!1,null,null,null),c=p.exports}}]);
-//# sourceMappingURL=664.37b3e545.js.map
+//# sourceMappingURL=664.db9e0577.js.map
